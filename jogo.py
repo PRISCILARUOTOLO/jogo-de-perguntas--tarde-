bd39df0292a5ec9a9d331c10a28a573877362c2d
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2025-11-05 16:53:21 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,3 +1,7 @@
 #Colocar título e descrever o jogo.
 print("JOGO DE PERGUNTAS")
+
 print("O jogo será trabalhado na forma de utilizar dados externos, para desenvolver respostas e novas perguntas, utilizando a idéia de INPUT")
+#Pensar em um tema...
+nome = input("Qual é seu nome?")
+print("Oi" ,nome)
