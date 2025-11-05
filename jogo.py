e86aa9d86064117ebcdb6572e8f8d58e6ac67428
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2025-11-05 16:34:23 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,3 @@
+#Colocar título e descrever o jogo.
+print("JOGO DE PERGUNTAS")
+print("O jogo será trabalhado na forma de utilizar dados externos, para desenvolver respostas e novas perguntas, utilizando a idéia de INPUT")
