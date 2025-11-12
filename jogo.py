1d4fefcef1b574d9db1194cfe63e89ca75171c54
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2025-11-12 16:54:49 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -5,3 +5,13 @@ print("O jogo será trabalhado na forma de utilizar dados externos, para desenvo
 #Pensar em um tema...
 nome = input("Qual é seu nome?")
 print("Oi" ,nome)
+
+pontos = 0;
+
+#pergunta1
+matemático = input("Qual foi o matemático que desenvolveu a seguinte fórmula, a² = b² + c²")
+if matemático == "Pitágoras" or matemático == "pitagoras":
+     print("Parabéns", nome,",você acertou.🤓")
+     pontos += 1
+else: 
+     print("Que pena", nome, "você errou 🤔")
